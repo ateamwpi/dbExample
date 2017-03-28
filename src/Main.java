a517a7f65e40fb47af214987e7f0a753616241a2
Matt McDonald <mattmcd25@gmail.com>
2017-03-28 12:24:20 -0400

    Finished add new entry

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-/**
- * Created by WilsonWong on 3/19/2017.
- */
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 public class Main {
 
@@ -19,7 +19,7 @@ public class Main {
         }
 
         System.out.println("Java DB driver registered!");
-        Connection connection = null;
+        Connection connection;
 
         try {
             // substitute your database name for myDB
@@ -33,14 +33,18 @@ public class Main {
                 System.out.print("Menu:\n(1) Add new entry\n(2) Print current entries\n(3) Re-create table\n(4) Quit\nChoose an option: ");
                 String in = sc.nextLine();
                 if(in.equals("1")) {
-                    String ID = "0";
-                    String name = "Allergy";
-                    String room = "4G";
-                    String locType = "Service";
-                    stmt.execute("INSERT INTO Location VALUES (0, 'Allergy', '4G', 'Service')");// + ID + ", " + name + ", " + room + ", " + locType + ")");
+                    System.out.print("Name of entry: ");
+                    String name = sc.nextLine();
+                    System.out.print("Room: ");
+                    String room = sc.nextLine();
+                    rset = stmt.executeQuery("SELECT * FROM Location ORDER BY locID DESC");
+                    rset.next();
+                    int ID = rset.getInt("locID") + 1;
+                    String locType = (name.contains(",") ? "Physician" : "Service");
+                    stmt.execute("INSERT INTO Location VALUES (" + ID + ", '" + name + "', '" + room + "', '" + locType + "')");
                 }
                 else if(in.equals("2")) {
-                    rset = stmt.executeQuery("SELECT * FROM Location");
+                    rset = stmt.executeQuery("SELECT * FROM Location ORDER BY locID ASC");
                     System.out.println(" ID|                                             Name |     Room |   locType");
                     System.out.println("---|--------------------------------------------------|----------|----------");
                     while (rset.next()) {
@@ -69,7 +73,6 @@ public class Main {
         } catch (SQLException e) {
             System.out.println("Connection failed. Check output console.");
             e.printStackTrace();
-            return;
         }
     }
 }
